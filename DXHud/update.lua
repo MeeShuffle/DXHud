ed8c553c1e3bd78882e3d11f35b5e7067541f7c9
Dawid <shufflelua@gmail.com>
2021-07-30 15:40:02 +0200

    Added update

--- DXHud/update.lua
+++ DXHud/update.lua
@@ -3,10 +3,10 @@
 
 
 
-addCommandHandler("updatecedit",function(plr)
-    print("[Custom_edit] Łączenie z GitHub...")
-    print("[Custom_edit] Przygotowywanie do pobierania...")
-    fetchRemote("https://raw.githubusercontent.com/sirphantasm/custom_edit/main/custom_edit/meta.xml",function(data,error,path)
+addCommandHandler("updatehud",function(plr)
+    print("[DXHud] Łączenie z GitHub...")
+    print("[DXHud] Przygotowywanie do pobierania...")
+    fetchRemote("https://raw.githubusercontent.com/sirphantasm/DXHud/main/DXHud/meta.xml",function(data,error,path)
         if error==0 then
             if fileExists("download.xml") then
                 fileDelete("download.xml")
@@ -14,7 +14,7 @@ addCommandHandler("updatecedit",function(plr)
             local file=fileCreate("download.xml")
             fileWrite(file,data)
             fileClose(file)
-            print("[Custom_edit] Lista plików pobrana, szykuję do pobrania...")
+            print("[DXHud] Lista plików pobrana, szykuję do pobrania...")
             prepare()
         end
     end,"",false,"meta.xml")
@@ -31,7 +31,7 @@ function prepare()
             table.insert(files,path)
         end
     end
-    print("[Custom_edit] Pliki przygotowane do pobrania...")
+    print("[DXHud] Pliki przygotowane do pobrania...")
     xmlUnloadFile(xml)
     fileCount=0
     download()
@@ -45,7 +45,7 @@ function download()
         finish()
         return
     end
-    fetchRemote("https://raw.githubusercontent.com/sirphantasm/custom_edit/main/custom_edit/"..files[fileCount],function(data,error,sciezka)
+    fetchRemote("https://raw.githubusercontent.com/sirphantasm/DXHud/main/DXHud/"..files[fileCount],function(data,error,sciezka)
         if error==0 then
             if fileExists(sciezka) then
                 fileDelete(sciezka)
@@ -61,7 +61,7 @@ end
 
 
 function finish()
-    print("Pobieranie zakończone, zrestartuj zasób custom_edit")
+    print("Pobieranie zakończone, zrestartuj zasób DXHud")
     local xml=fileOpen("download.xml")
     local size = fileGetSize(xml)
     local xml_content=fileRead(xml,size)
@@ -73,7 +73,7 @@ function finish()
     fileWrite(meta,xml_content)
     fileClose(meta)
     fileDelete("download.xml")
-    fetchRemote("https://raw.githubusercontent.com/sirphantasm/custom_edit/main/custom_edit/version.txt",function(data,error,sciezka)
+    fetchRemote("https://raw.githubusercontent.com/sirphantasm/DXHud/main/DXHud/version.txt",function(data,error,sciezka)
         if error==0 then
             if fileExists(sciezka) then
                 fileDelete(sciezka)
@@ -89,7 +89,7 @@ end
 
 
 function checkVersion()
-    fetchRemote("https://raw.githubusercontent.com/sirphantasm/custom_edit/main/custom_edit/version.txt",function(data,error,sciezka)
+    fetchRemote("https://raw.githubusercontent.com/sirphantasm/DXHud/main/DXHud/version.txt",function(data,error,sciezka)
         if error==0 then
             if not fileExists(sciezka) then
                 local file=fileCreate(sciezka)
@@ -99,12 +99,12 @@ function checkVersion()
                 local size=fileGetSize(file)
                 local content=fileRead(file,size)
                 if data~=content then
-                    print("Dostępna jest nowsza wersja custom_edit ("..data.."). Obecna wersja ("..content..").")
-                    print("Wpisz /updatecedit aby pobrać aktualizację")
+                    print("Dostępna jest nowsza wersja DXHud by sirphatnasm ("..data.."). Obecna wersja ("..content..").")
+                    print("Wpisz /updatehud aby pobrać aktualizację")
                 end
             end
         end
     end,"",false,"version.txt")
 end
 setTimer(checkVersion,60000,0)
-addCommandHandler("ceditwersja",checkVersion)
+addCommandHandler("hudwersja",checkVersion)
